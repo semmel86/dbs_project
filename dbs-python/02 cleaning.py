2ebaf456a5456d8bb88814afac50f204dd12a9ec
semmel86 <semmel1986@googlemail.com>
2017-06-01 20:20:07 +0200

    Update 02 cleaning.py

--- dbs-python/02 cleaning.py
+++ dbs-python/02 cleaning.py
@@ -5,33 +5,33 @@ with open('data.csv') as f:
     reader = list(csv.reader(f, delimiter=";"))
 
 cleaned = []
-deleted = 0
+adjusted = 0
 
 
 for row in reader[1:]:
     #check if handle, text, is_retweet, time, is_quote_status, retweet_count, favourite_count are null
     if not (row[0] and row[1] and row[2] and row[4] and row[6] and row[7] and row[8]):
-        deleted +=1
         continue
 
     #check if is_retweet==true but no original author or vice versa
     if row[2] == "True" and not row[3] or row[2] == "False" and row[3]:
-        deleted +=1
         continue
 
     #check if all dates have the same format
     try:
         strptime(row[4], "%Y-%m-%dT%H:%M:%S")
     except:
-        deleted +=1
         continue
 
-    if "$$" in row[1]:
-        deleted +=1
-        continue
+    #count how many texts are adjusted
+    if '\n' in row[1] or '""' in row[1]:
+        adjusted +=1
 
-    #replace paragraph with space
+    #replace paragraph with space and ""
     row[1] = row[1].replace("\n"," ")
+    row[1] = row[1].replace('""',"")
+
+
 
     # handle, text, is_retweet, original_author, time, is_quote_status, retweet_count, favorite_count
     cleaned.append([row[0], row[1], row[2], row[3], row[4], row[6], row[7], row[8]])
@@ -40,4 +40,5 @@ with open('cleaned_data.csv', "w", newline='') as f:
     writer = csv.writer(f, delimiter=";", quoting=csv.QUOTE_ALL)
     writer.writerows(cleaned)
 
-print(deleted)
+print('adjusted: '+str(adjusted))
+print ('deleted: '+str(len(reader)-len(cleaned)-1))
