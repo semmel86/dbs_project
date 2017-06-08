484c8f889213cc27a6a159b7fec655df9520d882
semmel86 <semmel1986@googlemail.com>
2017-06-08 14:14:39 +0200

    Create parseHeiseArticleTitles.py

--- parseHeiseArticleTitles.py
+++ parseHeiseArticleTitles.py
@@ -2,15 +2,24 @@
 from bs4 import BeautifulSoup
 import requests
 import operator
+'''
+@author Sebastian Selmke
+'''
 
 def getHeise(searchedPhrase,limit=10000):
-    
+    '''
+    function to get information about the titel of articles containing the searched word
+    @param searchedPhrase
+    @param limit
+    '''
+    # declaration
     available=True
     offset=0
     allTitles =[]
     allWords=[]
     dict={}
     
+    # inner function, grabbing the amount of available search results
     def getResultCount():
             url="https://www.heise.de/suche/?q="+searchedPhrase+"&search_submit.x=0&search_submit.y=0&rm=search&offset=0"
             response=requests.get(url)
@@ -26,10 +35,10 @@ def getHeise(searchedPhrase,limit=10000):
                 return n
             
             
-   # set a limit
+   # set a limit = the limit or the amount of articles
     results=getResultCount()
     if(results<limit):limit=results
-    
+    # iterate through the site with an offset of 20 results
     while(available==True):
         try:
             url="https://www.heise.de/suche/?q="+searchedPhrase+"&search_submit.x=0&search_submit.y=0&rm=search&offset="+str(offset)
@@ -53,15 +62,19 @@ def getHeise(searchedPhrase,limit=10000):
             available=False
             print(e)
 
-    
+    # parse the result list of words into an dictionary {'searchword':count}
     for n in range(len(allWords)):
+        
         if(dict.__contains__(allWords[n])):
+            #{'searchword':count+1}
             dict[allWords[n]]+=1
         else:
+            #new = {'searchword':1}
             dict.__setitem__(allWords[n], 1)
  
-           
+    #parse the dictionary into a list of tuples ('searchword',count) sorted by count ASC
     sorteddict = sorted(dict.items(), key=operator.itemgetter(1))
+    # print the last 3 list entrys and count
     print("Top 1:",sorteddict[-1][0],"\tAnzahl:",sorteddict[-1][1],"\nTop 2:",sorteddict[-2][0],"\tAnzahl:",sorteddict[-2][1],"\nTop 3:",sorteddict[-3][0],"\tAnzahl:",sorteddict[-3][1])
         
 
